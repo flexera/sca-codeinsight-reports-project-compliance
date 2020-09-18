b090705c76a735e84b4957e820eb32306a8fde20
Alex Rybak <arybak@flexera.com>
2020-09-18 13:41:15 -0700

    updated vuln widget to add spacing

--- report_artifacts.py
+++ report_artifacts.py
@@ -234,9 +234,12 @@ def generate_html_report(reportData):
         # Vulnerabilities
         html_ptr.write("            <td class='text-center text-nowrap' data-sort='%s' >\n" %numCriticalVulnerabilities)
         
-        # Write in single line to remove spaces between btn spans
         if numTotalVulnerabilities > 0:
-            html_ptr.write("                <span class='btn btn-critical'>%s</span><span class='btn btn-high'>%s</span><span class='btn btn-medium'>%s</span><span class='btn btn-low'>%s</span><span class='btn btn-none'>%s</span>\n" %(numCriticalVulnerabilities,numHighVulnerabilities,numMediumVulnerabilities, numLowVulnerabilities, numNoneVulnerabilities))
+            html_ptr.write("                <span class='btn btn-critical'>%s</span> \n" %(numCriticalVulnerabilities));
+            html_ptr.write("                <span class='btn btn-high'>%s</span> \n" %(numHighVulnerabilities));
+            html_ptr.write("                <span class='btn btn-medium'>%s</span> \n" %(numMediumVulnerabilities));
+            html_ptr.write("                <span class='btn btn-low'>%s</span> \n" %(numLowVulnerabilities));
+            html_ptr.write("                <span class='btn btn-none'>%s</span>\n" %(numNoneVulnerabilities))
         else:
             html_ptr.write("                <span class='btn btn-no-vulns'>0</span>\n")
 
